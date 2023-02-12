c19d088c44b32d3a1d660d4fe61cde6e8b608249
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2023-02-12 18:45:17 +0200

    Lection 15 init

---
+++ lection_15/task_2/index.js
@@ -0,0 +1,7 @@
+// Модуль 8. Заняття 15 Модульність коду
+
+// 4. Webpack та його налаштування. https://webpack.js.org/concepts/;
+// 4.1. Білд (producation i development);
+// 4.2. Власний аналог LiveServer (webpack-dev-server);
+// 4.3. Запустити html html-webpack-plugin;
+// 4.4. import & export;
