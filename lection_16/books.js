47fac745a6e80835b7479af23218a1477b10f649
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2023-02-16 21:59:48 +0200

    Lection 16 finish

--- lection_16/books.js
+++ lection_16/books.js
@@ -7,47 +7,47 @@ const guidGenerator = () => {
 
 const books = [
   {
-    id: guidGenerator(),
+    id: "1",
     name: "Product Ledership",
     url: "https://www.bringthedonuts.com/img/books/81W-nkY9Y-L.jpg",
   },
   {
-    id: guidGenerator(),
+    id: "2",
     name: "The Influential",
     url: "https://productcollective.com/wp-content/uploads/2021/03/51Gbe7zH50L._SX348_BO1204203200_.jpg",
   },
   {
-    id: guidGenerator(),
+    id: "3",
     name: "Continuous Discovery Habits",
     url: "https://images.ctfassets.net/lh3zuq09vnm2/1LLpKnARQNYDlV0p4bZ7De/5f76e773fd38bebfb6f8b28b9f8d71f9/Continuous_Discovery_Habits.jpg",
   },
   {
-    id: guidGenerator(),
+    id: "4",
     name: "Product Management",
     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNWljSFr5ztn3hCqp8x6MsJJMJw9wdqf-UM6TOulNZ6qg6MSwZkV9hiXz3IizNO-2-mec&usqp=CAU",
   },
   {
-    id: guidGenerator(),
+    id: "5",
     name: "API Product Management",
     url: "https://d2sofvawe08yqg.cloudfront.net/apiproductmanagement/s_hero2x?1620561555",
   },
   {
-    id: guidGenerator(),
+    id: "6",
     name: "The Product Samurai",
     url: "https://m.media-amazon.com/images/I/41C9M-qw6UL._SY264_BO1,204,203,200_QL40_ML2_.jpg",
   },
   {
-    id: guidGenerator(),
+    id: "7",
     name: "Crossing the Chasm",
     url: "https://images.ctfassets.net/lh3zuq09vnm2/5zrOoE9zeJCHWQTwkspqX7/ae9c5ed50c214c9bfd0ec4dc7f2ac35b/Crossing_the_Chasm.jpg",
   },
   {
-    id: guidGenerator(),
+    id: "8",
     name: "Start at the end",
     url: "https://m.media-amazon.com/images/I/81VaDa-uRDL._AC_UF1000,1000_QL80_.jpg",
   },
   {
-    id: guidGenerator(),
+    id: "9",
     name: "Getting things done",
     url: "https://www.bringthedonuts.com/img/books/81trXFlSkCL.jpg",
   },
--- lection_16/index.js
+++ lection_16/index.js
@@ -1,4 +1,18 @@
 //
+//
+
+// const showInfo = (type) => {
+//   if (type === "INFO") {
+//     //
+//     console.log("info");
+//   } else if (type === "DATA") {
+//     //
+//     console.log("data");
+//   } else {
+//     throw new Error("This type of info is not exist");
+//   }
+// };
+
 // Модуль 8 - Заняття 16 - Веб-сховище
 
 // =>
@@ -11,3 +25,120 @@
 //  * Створити сторінку з корзиною за допомогою JS;
 //  * Реалізувати функціонал вибору книжки;
 //  * Підєднати допомогу localStorage.
+
+class BookStorage {
+  static KEY = "SELECTED_BOOKS";
+  #selectedBooks = [];
+
+  #getBooksFromStorage() {
+    try {
+      this.#selectedBooks =
+        JSON.parse(localStorage.getItem(BookStorage.KEY)) || [];
+    } catch (_) {
+      this.#selectedBooks = [];
+    }
+  }
+
+  constructor() {
+    this.#getBooksFromStorage();
+  }
+
+  add = (book) => {
+    this.#selectedBooks.push(book);
+    localStorage.setItem(BookStorage.KEY, JSON.stringify(this.#selectedBooks));
+  };
+
+  hasBook = (book) => this.#selectedBooks.some(({ id }) => id === book.id);
+
+  get selectedBooks() {
+    return this.#selectedBooks;
+  }
+}
+
+const bookStorage = new BookStorage();
+
+const customCreator = (name, config = {}) => {
+  const element = document.createElement(name);
+
+  Object.entries(config).forEach(([key, value]) => {
+    element[key] = value;
+  });
+
+  return element;
+};
+
+const root = document.querySelector("#root");
+
+const createBookElements = (books, withAddButton) => {
+  return books.map((book) => {
+    const bookElement = customCreator("div", { className: "book" });
+
+    const bookImg = customCreator("img", { src: book.url });
+    const bookTitle = customCreator("h3", { textContent: book.name });
+
+    bookElement.append(bookImg, bookTitle);
+
+    if (withAddButton) {
+      const bookButton = customCreator("button", {
+        textContent: "Add to cart",
+      });
+
+      if (bookStorage.hasBook(book)) {
+        bookButton.disabled = true;
+        bookButton.textContent = "Added to card";
+      }
+
+      bookButton.addEventListener(
+        "click",
+        () => {
+          bookButton.disabled = true;
+          bookButton.textContent = "Added to card";
+          bookStorage.add(book);
+        },
+        { once: true }
+      );
+
+      bookElement.appendChild(bookButton);
+    }
+
+    return bookElement;
+  });
+};
+
+const createShopPage = () => {
+  const wrapper = customCreator("div", { className: "content" });
+
+  const bookElements = createBookElements(books, true);
+
+  const cart = customCreator("button", {
+    className: "cart",
+    textContent: "CART",
+  });
+
+  cart.addEventListener("click", createCartPage, { once: true });
+
+  wrapper.append(...bookElements, cart);
+
+  root.innerHTML = "";
+  root.appendChild(wrapper);
+};
+
+const createCartPage = () => {
+  const wrapper = customCreator("div", { className: "content" });
+
+  const bookElements = createBookElements(bookStorage.selectedBooks, false);
+
+  const back = customCreator("button", {
+    className: "back",
+    textContent: "BACK",
+  });
+
+  back.addEventListener("click", createShopPage, { once: true });
+
+  wrapper.append(...bookElements, back);
+
+  root.innerHTML = "";
+  root.appendChild(wrapper);
+};
+
+createShopPage();
