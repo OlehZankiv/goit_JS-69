0a4ea84c06fd232389c242019e365dbc836151db
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2023-02-25 22:08:00 +0200

    Lection 19 init

---
+++ lection_19/index.js
@@ -0,0 +1,17 @@
+//
+
+// THEORY
+// 1. HTTP / HTTPS
+// 2. REST API (method, options, path, body)
+// 3. GET, POST, PUT, DELETE
+// 4. Headers
+// 5. Paths
+// 6. Codes (1**, 2**, 3**, 4**, 5**)
+// 7. fetch (https://jsonplaceholder.typicode.com)
+// 8. parameters, URLSearchParams
+// 9. CORS
+
+// Модуль 10 - Заняття 19 - HTTP-запити
+// 1. The Pokedex API (fetch)
+// Написати функцію для стягнення картинок покемонів та їхніх даних.
+// Відображати їх за допомогою JS.
